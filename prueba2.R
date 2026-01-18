f4266717afc70dac9246ac751fee71d273e7b982
EduDerideS <ederide@gmail.com>
2026-01-18 15:02:46 -0300

    Initial commit

---
+++ prueba2.R
@@ -0,0 +1,17 @@
+#Este es un archivo de prueba
+
+install.packages(c("usethis", "renv", "gh"))
+usethis::use_git()
+usethis::use_github()
+gh::gh_whoami()
+
+# NO pegar tokens aquí
+token <- Sys.getenv("GITHUB_PAT")
+
+gitcreds::gitcreds_delete()
+gitcreds::gitcreds_set()
+
+git add probando.R
+git commit --amend --no-edit
+
+#Listo
